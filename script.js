563a8108589b7d7e8f7ab7b73db26e081d01a569
Anjali-3210 <anjalip3210@gmail.com>
2025-08-01 17:43:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6+1);
+var randomDice = "dice" + randomNumber1 + ".png";
+var randomImage = "images/" + randomDice;
+var img1 = document.querySelectorAll("img")[0];
+img1.setAttribute("src",randomImage);
+var randomNumber2 = Math.floor(Math.random()*6+1);
+var randomDice2 = "dice" + randomNumber2 + ".png";
+var randomImg2 = "images/" + randomDice2;
+var img2 = document.querySelectorAll("img")[1];
+img2.setAttribute("src", randomImg2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+}
+else{document.querySelector("h1").innerHTML = "It's a Draw!!"}
